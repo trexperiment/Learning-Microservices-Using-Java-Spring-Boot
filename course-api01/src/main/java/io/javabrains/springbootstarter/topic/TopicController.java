b18b886d10daac11e24fae4083888959cc02722e
Pradip Kumar Murmu <jayantasaha6@gmail.com>
2024-08-29 13:24:26 +0530

    Adding the feature of adding a topic using the controller /topics and implement the POST HTTP methods

--- course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -4,6 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +25,11 @@ public class TopicController {
 	@GetMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value="/topics")
+	public String addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+		return "Added Successfully.";
 	}
 }
