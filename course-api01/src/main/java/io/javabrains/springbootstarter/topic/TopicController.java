9b4e3811f5e9b775fc0dc8adaf4a6cd8c283c29b
Pradip Kumar Murmu <jayantasaha6@gmail.com>
2024-08-29 13:00:00 +0530

    Adding the feature of get one topic from the controller.

--- course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,20 +1,25 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
+	
 	@GetMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description"),
-				new Topic("javascript", "Javascript Framework", "Javascript Framework Description")
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@GetMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 }
