1f34faf07d426f7973472a34a91df8683df64b07
Pradip Kumar Murmu <jayantasaha6@gmail.com>
2024-08-29 13:43:25 +0530

    Adding the feature of adding a topic using the controller /topics/{id} and implement the PUT HTTP methods

--- course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -17,19 +17,30 @@ public class TopicController {
 	private TopicService topicService;
 	
 	
+	//GET method all topics
 	@GetMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
 	
+	// GET method using  an id
 	@GetMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
 	
+	//POST Update methods
 	@RequestMapping(method = RequestMethod.POST, value="/topics")
 	public String addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 		return "Added Successfully.";
 	}
+	
+	//PUT method
+	@RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
+	public String addTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id,topic);
+		return "Updated Successfully.";
+	}
+	
 }
--- course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ course-api01/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -29,4 +29,15 @@ public class TopicService {
 		topics.add(topic);
 		
 	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0; i< topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
 }
